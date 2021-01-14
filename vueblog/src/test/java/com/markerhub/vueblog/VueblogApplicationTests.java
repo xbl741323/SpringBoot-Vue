010af9df3106ea61910cf12224005b3b1575aff6
wt <1546766776@qq.com>
2021-01-14 11:56:07 +0800

    项目实战开始了

---
+++ vueblog/src/test/java/com/markerhub/vueblog/VueblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markerhub.vueblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
